45941aa688a67782092b1d1b3d3ae611f41eda8b
Geano1 <inthegeans@gmail.com>
2025-08-11 02:30:50 +0100

    Delete bubbles-cafe directory

--- bubbles-cafe/client_old_backup/src/components/background/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as PixelArtBackground } from './PixelArtBackground';
-export { default as PixelArtDemo } from './PixelArtDemo';
-export { default as PixelArtGenerator } from './PixelArtGenerator';
